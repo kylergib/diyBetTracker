ccc51a382fcbee61a99d098d45f7bf2648324230
kylergib <48994502+kylergib@users.noreply.github.com>
2023-08-10 08:42:17 -0400

    Initial commit

---
+++ src/main/java/com/kylergib/diybettracker/DiyBetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.kylergib.diybettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyBetTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiyBetTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kylergib/diybettracker/DiyBetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylergib.diybettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyBetTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
